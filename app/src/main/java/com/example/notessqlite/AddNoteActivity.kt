02940043a593e4153a18644a10c488d55df01598
Trần Trung Kiên <trungkien2981412@gmail.com>
2024-07-14 18:52:47 +0700

    check titlte null

--- app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
+++ app/src/main/java/com/example/notessqlite/AddNoteActivity.kt
@@ -2,29 +2,32 @@ package com.example.notessqlite
 
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.notessqlite.databinding.ActivityAddNoteBinding
 
 class AddNoteActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityAddNoteBinding
-    private lateinit var db : NoteDatabaseHelper
+    private lateinit var binding: ActivityAddNoteBinding
+    private lateinit var db: NoteDatabaseHelper
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityAddNoteBinding.inflate((layoutInflater))
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
         setContentView(binding.root)
         db = NoteDatabaseHelper(this)
-        binding.saveButton.setOnClickListener{
-            val  title = binding.titleEditText.text.toString()
-            val content = binding.contentEditText.text.toString()
-            val status = "pending"
-            val note = Note(0 , title , content, status)
-            db.insertNode(note)
-            finish()
-            Toast.makeText(this , "Note saved", Toast.LENGTH_SHORT).show()
+
+        binding.saveButton.setOnClickListener {
+            val title = binding.titleEditText.text.toString().trim()
+            val content = binding.contentEditText.text.toString().trim()
+
+            if (title.isEmpty()) {
+                Toast.makeText(this, "Please enter a title", Toast.LENGTH_SHORT).show()
+            } else {
+                val status = "pending"
+                val note = Note(0, title, content, status)
+                db.insertNode(note)
+                Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
     }
 }
